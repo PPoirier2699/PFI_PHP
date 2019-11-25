92bb8b10f126cd604c3ea2de67a5f9d37d373039
PPoirier2699 <57813534+PPoirier2699@users.noreply.github.com>
2019-11-25 11:57:25 -0500

    Add files via upload

---
+++ CLASSES/ALBUM/album.php
@@ -0,0 +1,87 @@
+<?php
+
+include_once __DIR__ . "/userTDG.php";
+
+class User{
+
+    private $id;
+    private $title;
+    private $authorID;
+    private $description;
+    private $creationTime;
+
+    
+    public function __construct(){
+       
+    }
+
+
+    //getters
+    public function get_id(){
+        return $this->id;
+    }
+
+    public function get_title(){
+        return $this->title;
+    }
+
+    public function get_authorID (){
+        return $this->authorID;
+    }
+
+    public function get_description(){
+        return $this->description;
+    }
+
+    public function get_creationTime(){
+        return $this->creationTime;
+    }
+
+
+    //setters
+    public function set_id($id){
+        $this->id = $id;
+    }
+
+    public function set_title($title){
+        $this->title = $title;
+    }
+
+    public function set_authorID($aID){
+        $this->authorID = $aID;
+    }
+
+    public function set_description($desc){
+        $this->description = $desc;
+    }
+
+    public function set_creationTime($ct){
+        $this->description = $ct;
+    }
+
+
+    /*
+        Quality of Life methods (Dans la langue de shakespear (ou QOLM pour les intimes))
+    */
+    public function load_album($id){
+        $TDG = AlbumTDG::getInstance();
+        $res = $TDG->get_by_id($id);
+
+        if(!$res)
+        {
+            $TDG = null;
+            return false;
+        }
+
+        $this->id = $res['id'];
+        $this->title = $res['title'];
+        $this->authorID = $res['authorID'];
+        $this->description = $res['description'];
+        $this->creationTime = $res['creationTime'];
+        
+        $TDG = null;
+        return true;
+    }
+}
+
+
---
+++ CLASSES/ALBUM/albumTDG.php
@@ -0,0 +1,151 @@
+<?php
+
+include_once __DIR__ . "/../../UTILS/connector.php";
+
+class AlbumTDG extends DBAO{
+
+    private $tableName;
+    private static $_instance = null;
+
+    public function __construct(){
+        
+        $this->tableName = "albums";
+        
+    }
+    public static function getInstance()
+    {
+        if(is_null(self::$_instance))
+        {
+            self::$_instance = new AlbumTDG();  
+        }
+        return self::$_instance;
+    }
+    //create table
+    public function createTable(){
+        
+        try{
+            $conn = $this->connect();
+            $query = "CREATE TABLE IF NOT EXISTS albums (id INTEGER(10) AUTO INCREMENT PRIMARY KEY,
+            title VARCHAR(50) NOT NULL,
+            authorID INTEGER(10) NOT NULL,
+            description LONGTEXT NOT NULL,
+            creationTime DATE NOT NULL)";
+            $stmt = $conn->prepare($query);
+            $stmt->execute();
+            $resp = true;
+        }
+
+        //error catch and msg display
+        catch(PDOException $e)
+        {
+            $resp = false;
+        }
+        //fermeture de connection PDO
+        $conn = null;
+        return $resp;
+    }
+    //drop table
+    public function drop_table(){
+        
+        try{
+            $conn = $this->connect();
+            $query = "DROP TABLE albums";
+            $stmt = $conn->prepare($query);
+            $stmt->execute();
+            $resp = true;
+        }
+
+        //error catch and msg display
+        catch(PDOException $e)
+        {
+            $resp = false;
+        }
+        //fermeture de connection PDO
+        $conn = null;
+        return $resp;
+    }
+    public function get_by_id($id){
+        
+        try{
+            $conn = $this->connect();
+            $query = "SELECT id, title, authorID, description, creationTime FROM albums WHERE id=:id";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $result = $stmt->fetch();
+        }
+        
+        catch(PDOException $e)
+        {
+            echo "Error: " . $e->getMessage();
+        }
+        //fermeture de connection PDO
+        $conn = null;
+        return $result;
+    }
+    public function get_by_title($title){
+        
+        try{
+            $conn = $this->connect();
+            $query = "SELECT id, title, authorID, description, creationTime FROM albums WHERE title=:title";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(':title', $title);
+            $stmt->execute();
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $result = $stmt->fetch();
+        }
+        
+        catch(PDOException $e)
+        {
+            echo "Error: " . $e->getMessage();
+        }
+        //fermeture de connection PDO
+        $conn = null;
+        return $result;
+    }
+    public function get_by_authorID($aID){
+        
+        try{
+            $conn = $this->connect();
+            $tableName = $this->tableName;
+            $query = "SELECT id, title, authorID, description, creationTime FROM albums WHERE auhtorID=:aID";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(':aID', $aID);
+            $stmt->execute();
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $result = $stmt->fetchAll();
+        }
+        
+        catch(PDOException $e)
+        {
+            echo "Error: " . $e->getMessage();
+        }
+        //fermeture de connection PDO
+        $conn = null;
+        return $result;
+    }
+    public function add_album($title, $authorID, $description, $creationTime){
+        
+        try{
+            $conn = $this->connect();
+            $tableName = $this->tableName;
+            $query = "INSERT INTO albums (title, authorID, description, creationTime) VALUES (:title, :authorID, :description, :creationTime)";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(':title', $title);
+            $stmt->bindParam(':authorID', $authorID);
+            $stmt->bindParam(':description', $description);
+            $stmt->bindParam(':creationTime', $creationTime);
+            $stmt->execute();
+            $resp =  true;
+        }
+        
+        catch(PDOException $e)
+        {
+            $resp =  false;
+        }
+        //fermeture de connection PDO
+        $conn = null;
+        return $resp;
+    }
+}
