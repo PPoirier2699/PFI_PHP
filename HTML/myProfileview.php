67e24ad7606e8ffc5786a29ed3602df4b34c6e5b
MarcAndreHebert <58182354+MarcAndreHebert@users.noreply.github.com>
2019-11-28 16:34:20 -0500

    Add files via upload

--- HTML/myProfileview.php
+++ HTML/myProfileview.php
@@ -7,13 +7,13 @@ if(!isset($_SESSION['userID'])){
 
 <main>
     <h2 style="text-align:center;">Welcome To Your Profile</h2>
-    <form action="" method="post">
+    <form action="DOMAINLOGIC/updateinfo.dom.php" method="post">
         <div id="infoProfile" class="border p-4" style="border-radius: 15px;">
             <img id="profilepic" class="border" src="<?php echo $_SESSION["profilePicPath"]?>" alt="Your profile pic">   
-            <div>Username : <input class="form-control" value='<?php echo $_SESSION["userName"]?>'></div>
-            <div>E-Mail : <input class="form-control" value='<?php echo $_SESSION["userEmail"]?>'></div>
-            <div>Password : <input class="form-control" ></div>
-            <div>Confirm Password : <input class="form-control" ></div>
+            <div>Username : <input name="username" class="form-control" value='<?php echo $_SESSION["userName"]?>'></div>
+            <div>E-Mail : <input name="email" class="form-control" value='<?php echo $_SESSION["userEmail"]?>'></div>
+            <div>Password : <input name="pw" class="form-control" ></div>
+            <div>Confirm Password : <input name="pwValidation" class="form-control" ></div>
 
             <button class="btn btn-success mb-sm-3" type="submit">Submit Changes</button>
         </div>
