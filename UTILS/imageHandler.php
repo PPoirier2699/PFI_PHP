977246e7f5878b01f24f9f9ca21f791688b5df2e
MarcAndreHebert <58182354+MarcAndreHebert@users.noreply.github.com>
2019-11-28 19:42:15 -0500

    Add files via upload

---
+++ UTILS/imageHandler.php
@@ -0,0 +1,36 @@
+<?php
+class ImageHandler {
+    public static function FileToImageURL($file) {
+        //check if the image is a fake image
+        if (!getimagesize($file['tmp_name'])) {
+            header("Location: ../mainPage.php?ErrorMSG=This%20is not%20a%20real%20image!");
+            die();
+        }
+
+
+        $target_dir = "IMG/";
+
+        //obtenir l'extention du fichier uploader
+        $media_file_type = pathinfo($file['name'], PATHINFO_EXTENSION);
+
+        // Valid file extensions
+        $img_extensions_arr = array("jpg", "jpeg", "png", "gif");
+
+        if (!in_array($media_file_type, $img_extensions_arr)) {
+            return false;
+        }
+
+        //creation d'un nom unique pour la "PATH" du fichier
+        $path = tempnam("../IMG", '');
+        unlink($path);
+        $file_name = basename($path, ".tmp");
+        //creation de l'url pour la DB
+        $url = $target_dir . $file_name . "." . $media_file_type;
+        //deplacement du fichier uploader vers le bon repertoire (Medias)
+        move_uploaded_file($file['tmp_name'], "../" . $url);
+
+        return $url;
+    }  
+}
+
+?>
