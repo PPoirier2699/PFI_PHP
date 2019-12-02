a7c9e990e1fc2b132c18ad0bd7ee804b52754012
charleslefebvre <58051166+charleslefebvre@users.noreply.github.com>
2019-12-02 11:56:26 -0500

    Update mainPageview.php

--- HTML/mainPageview.php
+++ HTML/mainPageview.php
@@ -1,1 +1,12 @@
 <h2>Main page content</h2>
+
+<?php
+    include_once "./CLASSES/ALBUM/albumTDG.php";
+
+    $albumTDG = AlbumTDG::getInstance();
+    
+    $res = $albumTDG->get_top_album(4);
+    $albumTDG->display_albums($res);
+
+?>
+
