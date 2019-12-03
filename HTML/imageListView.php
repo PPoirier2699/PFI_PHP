92818b4a8d4989b19265af136c720ddf1d297f9d
MarcAndreHebert <58182354+MarcAndreHebert@users.noreply.github.com>
2019-12-03 12:02:37 -0500

    Add files via upload

--- HTML/imageListView.php
+++ HTML/imageListView.php
@@ -1,33 +1,51 @@
+<?php
+    include_once __DIR__ . "/../CLASSES/IMAGE/image.php";
+    include_once __DIR__ . "/../CLASSES/ALBUM/album.php";
+
+    $albumID;
+    if (isset($_GET["albumID"])) {
+        $albumID = $_GET["albumID"];
+    } else {
+        header("Location: ../login.php?ErrorMSG=No albumID provided");
+        die();
+    }
+
+    $img = new Image();
+    $album = new Album();
+
+    $album = $album->get_album($albumID);
+    //Set la variable de session avec les images de l'album courantes(que lutilisateur a clicke dessus ou autre)
+    $img = $img->get_all_image_by_album($albumID);
+    ?>
 <main role="main">
 
   <section class="jumbotron text-center">
     <div class="container">
-      <h1><?php echo $_SESSION["currentAlbum"]["title"];?></h1>
+      <h1><?php echo $album["title"];?></h1>
     </div>
   </section>
 
-  <div class="albucurrentAlbumTitlem py-5 bg-light">
+  <div class="py-5 bg-light">
     <div class="container">
 
       <div class="row">
 
       <?php
 
-        foreach($_SESSION["currentAlbumImage"] as $key => $value){
-            $_SESSION["temp"] = $value;
+        foreach($img as $key => $value){
             ?>
             <div class="col-md-4">
           <div class="card mb-4 shadow-sm">
-            <img src="<?php echo $_SESSION["temp"]["url"]?>" class="bd-placeholder-img card-img-top" width="100%" height="225" alt="an image of the album">
+            <img src="<?php echo $value["url"]?>" class="bd-placeholder-img card-img-top" width="100%" height="225" alt="an image of the album">
             <div class="card-body">
-              <p class="card-text"><?php echo $_SESSION["temp"]["description"]?></p>
+              <p class="card-text"><?php echo $value["description"]?></p>
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
                   <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
                   <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
                   <button type="button" class="btn btn-sm btn-outline-secondary">Like</button>
                 </div>
-                <small class="text-muted"><?php echo $_SESSION["temp"]["creationTime"]?></small>
+                <small class="text-muted"><?php echo $value["creationTime"]?></small>
               </div>
             </div>
           </div>
@@ -35,11 +53,15 @@
             <?php
         }
         ?></div><?php
-        if(validate_session() && $_SESSION["currentAlbum"]["authorID"] == $_SESSION["userID"]) {
+        if(validate_session() && $album["authorID"] == $_SESSION["userID"]) {
           ?>          
-          <form action="" method="post" enctype="multipart/form-data">
+          <form action="DOMAINLOGIC/addImage.dom.php" method="post" enctype="multipart/form-data">
+            <input type="text" style="display:none;" name="albumID" value="<?php echo $albumID; ?>">
+            <input type="text" style="display:none;" name="authorID" value="<?php echo $album["authorID"]; ?>">
             <input class="fileinput" type="file" name="addpicture" id="addpicture">
-            <label for="addpicture">Add a picture to the album</label>
+            <label class="btn btn-primary" for="addpicture">Add a picture to the album</label>
+            <textarea class="form-control" name="description" id="descaddimage" cols="30" rows="5" placeholder="Add Your Description Here..."></textarea>
+            <button class="btn btn-primary" type="submit" style="margin-top: 5%; margin-left:30%; margin-right:30%; width:40%;">Submit</button>
           </form>
           <?php
         }
