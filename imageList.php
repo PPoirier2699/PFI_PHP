f1f4a02df107a9f4c430924e86460c70238c4816
MarcAndreHebert <58182354+MarcAndreHebert@users.noreply.github.com>
2019-12-03 12:02:08 -0500

    Add files via upload

--- imageList.php
+++ imageList.php
@@ -1,15 +1,15 @@
 <?php
     include_once __DIR__ . "/UTILS/sessionHandler.php";
+
+
     session_start();
-
-
     //load view content
     $module = "imageListView.php";
     $content = array();
     array_push($content, $module);
     
     //variables used in the loaded module
-    $title = "Login";
+    $title = "Album";
 
     //load the masterpage
     require_once __DIR__ . "/HTML/masterpage.php";
