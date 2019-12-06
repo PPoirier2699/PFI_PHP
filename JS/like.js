026cb6251fb58978dbcd7ac3c1c2bcea1d9215bf
charleslefebvre <58051166+charleslefebvre@users.noreply.github.com>
2019-12-05 21:25:16 -0500

    Add files via upload

---
+++ JS/like.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    let likes = $("button[id^='IMG']");
+    for(let i = 0; i < likes.length; ++i){
+        likes[i].onclick = function(){
+            likes[i].classList.toggle("btn-primary");
+            likes[i].classList.toggle("btn-outline-secondary");
+        }
+    }
+});
