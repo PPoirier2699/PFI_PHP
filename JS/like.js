43ce6cb880cace9b63b21a8cd8522cf76e7100aa
charleslefebvre <58051166+charleslefebvre@users.noreply.github.com>
2019-12-07 13:04:21 -0500

    Update like.js

--- JS/like.js
+++ JS/like.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
         likes[i].onclick = function(){
             likes[i].classList.toggle("btn-primary");
             likes[i].classList.toggle("btn-outline-secondary");
+            location = 'DOMAINLOGIC/imageLike.dom.php';
         }
     }
 });
