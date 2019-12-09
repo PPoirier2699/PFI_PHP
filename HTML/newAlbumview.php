71e46c7ecc0d7a5c866b8bfee238599b125f893a
MarcAndreHebert <marcandre.hebert@live.fr>
2019-12-09 06:01:15 -0500

    style

--- HTML/newAlbumview.php
+++ HTML/newAlbumview.php
@@ -7,23 +7,20 @@
         echo "<script>alert('$error');</script>";
     }
 ?>
-<div class='form-group shadow-textarea p-4' style='margin: 90px 490px 0px -80px; display: inline-block; width: 50%;'>
-<h3>Create a new Album</h3><br>
-<form action='DOMAINLOGIC/newAlbum.dom.php' method='post' enctype="multipart/form-data">
-    <input class='form-control mb-2' placeholder='Title' name='title'>
-    <textarea class='form-control z-depth-1' rows='4' placeholder='Description' name='descriptionAlbum'></textarea><br>
-    <input type="text" style="display:none;" name="authorID" value="<?php echo $_SESSION["userID"]; ?>">
-    <input class="fileinput" type="file" name="firstImage" id="addpicture" onchange="readURL(this);"><br>
-    <label class="btn btn-secondary" for="addpicture">Add the first picture of the album</label>
-    <textarea class="form-control" name="descriptionImage" id="descaddimage" cols="30" rows="5" placeholder="Add Your Description Here..."></textarea><br>
-    
-    <button class="btn btn-primary btn-block" type="submit" >Submit</button>
-    
-</form>
-<div class="border p-2" style='text-align: center; float: right; margin-top: -212px; margin-right: -200px; height: 15%; width: 35%;'>
-    <p>Image preview</p>
-    <img class="imageModal" id="imageView" src="#" alt='Image' style="display: none;">
-</div>
+<h3 style="text-align:center;">Create a new Album</h3><br>
+<div class="row" style="padding:2.5%; width:100%;position: relative;">
+    <form class="row card-header" style="margin:0 2%; width:50%;" action='DOMAINLOGIC/newAlbum.dom.php' method='post' enctype="multipart/form-data">
+        <input class='form-control mb-2' placeholder='Title' name='title'>
+        <textarea class='form-control z-depth-1' rows='4' placeholder='Description' name='descriptionAlbum'></textarea><br>
+        <input type="text" style="display:none;" name="authorID" value="<?php echo $_SESSION["userID"]; ?>">
+        <input class="fileinput" type="file" name="firstImage" id="addpicture" onchange="readURL(this);"><br>
+        <label class="btn btn-primary" style="margin: 5% 15%; width:70%;" for="addpicture">Add the first picture of the album</label>
+        
+        <button class="btn btn-primary" type="submit" style="margin-top: 5%; margin-left:30%; margin-right:30%; width:50%;">Submit</button>    
+    </form>
+    <div class="col" style="width:50%; padding:1%; margin:0 2%; position:relative;">
+        <img style="position: absolute; top: 0; bottom: 0; margin: auto;" class="bd-placeholder-img card-img-top imageModal" id="imageView" src="IMG/preview.png" alt='Image'>
+    </div>
 </div>
 
 <!-- The Modal -->
