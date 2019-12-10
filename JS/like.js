ebc238deea767e88835d1bac549e2c8bd5954e79
charleslefebvre <58051166+charleslefebvre@users.noreply.github.com>
2019-12-09 23:13:13 -0500

    Add files via upload

--- JS/like.js
+++ JS/like.js
@@ -2,9 +2,20 @@ $(document).ready(function(){
     let likes = $("button[id^='IMG']");
     for(let i = 0; i < likes.length; ++i){
         likes[i].onclick = function(){
+            let action;
+            if(likes[i].classList.contains('btn-primary')){
+                action = 'remove';
+            }
+            else{
+                action = 'add';
+            }
             likes[i].classList.toggle("btn-primary");
-            likes[i].classList.toggle("btn-outline-secondary");
-            location = 'DOMAINLOGIC/imageLike.dom.php';
+            likes[i].classList.toggle("btn-outline-secondary");   
+            let imgID = likes[i].id.substring(3);
+            $(`#${imgID}`).load('DOMAINLOGIC/imageLike.dom.php' ,{
+                imgID: imgID,
+                action: action
+            });
         }
     }
 });
