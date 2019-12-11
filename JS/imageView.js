46b808ee0de809805e69b657d32baed0804df1cc
MarcAndreHebert <58182354+MarcAndreHebert@users.noreply.github.com>
2019-12-10 21:10:53 -0500

    Add files via upload

--- JS/imageView.js
+++ JS/imageView.js
@@ -1,17 +1,37 @@
 $(document).ready(function(){
-  var modal = document.getElementById("myModal");
-
   // Get the image and insert it inside the modal - use its "alt" text as a caption$(document).ready(function(){
   var modal = document.getElementById("myModal");
 
   // Get the image and insert it inside the modal - use its "alt" text as a caption
   var modalImg = $("#img01");
-  var captionText = document.getElementById("caption");
   $('.imageModal').click(function(){
       modal.style.display = "block";
       var newSrc = this.src;
+      var id = this.title;
+      var type = $('#objectType').val();
+      modalImg.attr('title', id);
+      $('#imageIDPostComment').attr('value', id);
       modalImg.attr('src', newSrc);
-      captionText.innerHTML = this.alt
+
+      $("#commentForJS").load("HTML/commentView.php" , {
+        objectID : id,
+        objectType : type
+      });
+      $()
+      $("#addCommentButton").click(function() {
+        var content = $('#contentComment').val();
+        $('#contentComment').val("");
+
+        $("#commentForJS").load("DOMAINLOGIC/addComment.dom.php" , {
+          objectID : id,
+          objectType : type,
+          content : content
+        });
+        $("#commentForJS").load("HTML/commentView.php" , {
+          objectID : id,
+          objectType : type
+        });
+      });
   });
   
   // Get the <span> element that closes the modal
@@ -22,19 +42,4 @@ $(document).ready(function(){
     modal.style.display = "none";
   }
 });
-  var modalImg = $("#img01");
-  var captionText = document.getElementById("caption");
-  $('.imageModal').click(function(){
-      modal.style.display = "block";
-      var newSrc = this.src;
-      modalImg.attr('src', newSrc);
-      captionText.innerHTML = this.alt
-  });
   
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
-  
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
