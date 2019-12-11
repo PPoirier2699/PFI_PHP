5133407483b22b16639c1d3d4f536428070624d2
MarcAndreHebert <58182354+MarcAndreHebert@users.noreply.github.com>
2019-12-11 09:27:29 -0500

    Add files via upload

--- JS/imageView.js
+++ JS/imageView.js
@@ -4,34 +4,37 @@ $(document).ready(function(){
 
   // Get the image and insert it inside the modal - use its "alt" text as a caption
   var modalImg = $("#img01");
+  var id;
+  var type;
   $('.imageModal').click(function(){
       modal.style.display = "block";
       var newSrc = this.src;
-      var id = this.title;
-      var type = $('#objectType').val();
+      id = this.title;
+      type = $('#objectType').val();
       modalImg.attr('title', id);
       $('#imageIDPostComment').attr('value', id);
-      modalImg.attr('src', newSrc);
+      modalImg.attr('src', newSrc);      
 
       $("#commentForJS").load("HTML/commentView.php" , {
         objectID : id,
         objectType : type
       });
-      $()
-      $("#addCommentButton").click(function() {
-        var content = $('#contentComment').val();
-        $('#contentComment').val("");
+  });
+  $("#addCommentButton").click(function() {
+    var content = $('#contentComment').val();
+    $('#contentComment').val("");
 
-        $("#commentForJS").load("DOMAINLOGIC/addComment.dom.php" , {
-          objectID : id,
-          objectType : type,
-          content : content
-        });
-        $("#commentForJS").load("HTML/commentView.php" , {
-          objectID : id,
-          objectType : type
-        });
+    $("#commentForJS").load("DOMAINLOGIC/addComment.dom.php" , {
+      objectID : id,
+      objectType : type,
+      content : content
+    }, function() {
+      $("#commentForJS").load("HTML/commentView.php" , {
+        objectID : id,
+        objectType : type
       });
+    });
+    
   });
   
   // Get the <span> element that closes the modal
