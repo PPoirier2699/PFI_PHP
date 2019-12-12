5c78f81eda7c7fb6d18c4484ca9ed54546cbabe9
charleslefebvre <58051166+charleslefebvre@users.noreply.github.com>
2019-12-12 17:44:39 -0500

    Add files via upload

--- HTML/searchview.php
+++ HTML/searchview.php
@@ -1,4 +1,5 @@
-<script src="JS\loadSearchAlbum.js"></script>
+<script src="JS\loadSearchMedia.js"></script>
+<script src="JS\imageView.js"></script>
 <?php
 
 include_once "./CLASSES/ALBUM/album.php";
@@ -12,17 +13,18 @@ $user = new User;
 $searchdWord = $_POST['search'];
 
 $albumsRes = $album->search_album($searchdWord,3);
-//$images = $imgTDG->search_image($searchdWord);
-$usersRes = $user->search_user($searchdWord);
+$imagesRes = $img->search_image($searchdWord,3);
+$usersRes = $user->search_user($searchdWord,3);
 ?>
 <input id="searchWord" style="display: none;" value="<?php echo $_POST['search']?>">
 <h1>ALBUMS</h1><br><hr><br>
 <div id="albums" class="p-4 border" style="position: relative;">
 	<?php $album->display_album_search($albumsRes); ?>	
+	<div id="message"></div>
 </div><br>
 <h1>IMAGES</h1><br><hr><br>
 <div id="images" class="p-4 border" style="position: relative;">
-	<!--<?php $album->display_album_search($albumsRes); ?>-->
+	<?php $img->display_image_search($imagesRes); ?>
 </div><br>
 <h1>USERS</h1><br><hr><br>
 <div id="users" class="p-4 border" style="position: relative;">
