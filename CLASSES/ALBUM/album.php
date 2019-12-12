2cf051c325c97cd2aebff1b1bff0bc212afa44cf
charleslefebvre <58051166+charleslefebvre@users.noreply.github.com>
2019-12-12 14:43:17 -0500

    Add files via upload

--- CLASSES/ALBUM/album.php
+++ CLASSES/ALBUM/album.php
@@ -95,6 +95,49 @@ class Album{
         $TDG = null;
         return $res;
     }
+    public function get_top_album($newAlbumCount){
+        
+        $TDG = AlbumTDG::getInstance();
+        return $TDG->get_top_album($newAlbumCount);
+    }
+    public function display_albums($res){
+        if(!empty($res)){      
+            foreach($res as $info){
+                echo "<div class='container border w-75 p-3 mt-5' style='float: left'>";
+                echo "<p><a style='text-decoration: none; color: black; font-size: 20px;' href=''#>" . $info['username'] . "</a></p>";
+                echo "<p><a style='text-decoration: none; color: black; font-size: 20px;' href='imageList.php?albumID=" . $info['id'] . "'>" . $info['title'] . "</a></p>";
+                echo "<img src='" . $info['url']. "' alt='img' height='100'>";
+                echo "<p class='lead'>Description: " . $info['description'] . "</p>";
+                echo "<p class='lead'>" . $info['creationTime'] . "</p>";
+                echo "</div>";   
+            }                                 
+        }      
+    }
+    public function no_more_albums_to_display($albumNewCount,$res){
+        if($albumNewCount > count($res)){
+            echo "<h6 style='position: absolute; left: 3%; bottom: 5%'>No more albums</h6>";
+            echo"<script>$('#moreAlbums').remove();</script>";
+        }
+    }
+    public function search_album($like,$newAlbumCount){
+        $TDG = AlbumTDG::getInstance();
+        return $TDG->search_album($like,$newAlbumCount);
+    }
+    public function display_album_search($res){
+        if(empty($res)){
+            echo "<h4>No albums corresponding to the research!</h4>";
+        }
+        else{            
+            foreach($res as $results){
+                echo "<h5 class='d-inline'><a style='text-decoration: none; color: black' href='imageList.php?albumID=" . $results['id'] . "'>" . $results['title'] . "</a></h5>";
+                echo "<a class='btn btn-primary' style='float: right' href='imageList.php?albumID=" . $results['id'] ."'>View album</a>";         
+                echo "<p class='lead'>" . $results['description'] . "</p>";
+                echo "<p class='lead'>" . $results['creationTime'] . "</p>";
+                echo "<br>";
+            }
+            echo "<br><button class='btn btn-light' style='position: absolute; left: 2%; bottom: 5%;' id='moreAlbums'>More albums</button>";
+        }
+    }
 }
 
 
