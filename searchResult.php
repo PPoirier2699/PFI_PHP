b2c4a0fe07542788d1f6f7840e09ddcd1a4de6bd
charleslefebvre <58051166+charleslefebvre@users.noreply.github.com>
2019-12-12 17:48:49 -0500

    Update searchResult.php

--- searchResult.php
+++ searchResult.php
@@ -1,11 +1,15 @@
 <?php 
+    include_once __DIR__ . "/UTILS/sessionHandler.php";
+    
     session_start();
 
     $title = "Search result";
     $module = "searchview.php";
+    $module2 = "modalView.php";
 
     $content = array();
     array_push($content, $module);
+    array_push($content, $module2);
 
     require_once __DIR__ . "/HTML/masterpage.php";
 
