9d4fe6cf0ab92dbd86f85642d7df78dae93bac4d
MarcAndreHebert <58182354+MarcAndreHebert@users.noreply.github.com>
2019-12-13 13:26:49 -0500

    Add files via upload

--- HTML/modalView.php
+++ HTML/modalView.php
@@ -11,19 +11,23 @@
     <img class="modal-image" id="img01">
 
     <div id="commentForJS" class="commentParent overflow-auto"></div>
-    <input type="text" name="objectType" id="objectType" value="image" style="display:none;">
-    <input type="text" name="objectID" value="" id="imageIDPostComment" style="display:none;">
 
-    <?php if(validate_session()) {?>
+    <input type="text" name="objectType" id="objectType" value="comment" style="display:none;">
+    <input type="text" name="objectID" value="" id="imageIDPostComment" style="display:none;">
     <div class="commentParent">
+    
+    
+    <?php if(validate_session()) {?>
+    
       <div class="comment">
           <div style="float: left;font-weight: bold;"><?php echo $_SESSION["userName"] ?></div>
           <textarea id="contentComment" name="content" class="form-control" rows="2"></textarea>
           <button class="btn btn-primary" type="submit" id="addCommentButton" style="font-size:12;margin-top:0;margin-bottom:0; margin-left:30%; margin-right:30%; width:40%;">Add Comment</button>
           
       </div>
-    </div>
+    
     <?php } ?>
+    </div>
   </div>
 
 
