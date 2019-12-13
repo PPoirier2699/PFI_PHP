024df8f63992e5091ef67fbfef28bb857f2e69a4
MarcAndreHebert <58182354+MarcAndreHebert@users.noreply.github.com>
2019-12-13 14:25:17 -0500

    Add files via upload

--- HTML/mainPageview.php
+++ HTML/mainPageview.php
@@ -1,20 +1,25 @@
 <script src="JS\loadFeedAlbums.js"></script>
+<script src="JS\imageView.js"></script>
 <h2>Main page content</h2>
 <div id="albums">
 <?php
     include_once "./CLASSES/ALBUM/album.php";
+    include_once "./UTILS/sessionHandler.php";
 
     $album = new Album;
     
     $res = $album->get_top_album(4);
     if(!empty($res)){      
         foreach($res as $info){ ?>
-            <div class='container border w-75 p-3 mt-5' style='float: left'>
+            <div id="commentForJS" class='container border w-75 p-3 mt-5' style='float: left'>
             <p><a style='text-decoration: none; color: black; font-size: 20px;' href='#'><?php echo $info['username'] ?></a></p>
             <p><a style='text-decoration: none; color: black; font-size: 20px;' href='imageList.php?albumID=<?php echo $info['id'] ?>'><?php echo $info['title'] ?></a></p>
             <img src='<?php echo $info['url'] ?>' alt='img' height='100'>
             <p class='lead'>Description: <?php echo $info['description'] ?></p>
             <p class='lead'> <?php $info['creationTime'] ?></p>
+            <?php if(validate_session() && $info["username"] == $_SESSION["userName"]) {?>				      
+                <button onClick="deleteFunc(<?php echo $info['id']?>, 'album');" type="button" class="btn">Delete</button>
+            <?php } ?>
             </div>
         <?php }                                 
     }   
