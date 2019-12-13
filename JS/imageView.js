476dfc9fc0806a4f8cf518d9083100c352e392b9
MarcAndreHebert <58182354+MarcAndreHebert@users.noreply.github.com>
2019-12-13 13:26:14 -0500

    Add files via upload

--- JS/imageView.js
+++ JS/imageView.js
@@ -1,11 +1,11 @@
+var id;
+var type;
+var commentCount = 5;
 $(document).ready(function(){
   // Get the image and insert it inside the modal - use its "alt" text as a caption$(document).ready(function(){
   var modal = document.getElementById("myModal");
-
   // Get the image and insert it inside the modal - use its "alt" text as a caption
   var modalImg = $("#img01");
-  var id;
-  var type;
   $('.imageModal').click(function(){
       modal.style.display = "block";
       var newSrc = this.src;
@@ -15,10 +15,7 @@ $(document).ready(function(){
       $('#imageIDPostComment').attr('value', id);
       modalImg.attr('src', newSrc);      
 
-      $("#commentForJS").load("HTML/commentView.php" , {
-        objectID : id,
-        objectType : type
-      });
+      load_comment(commentCount, id, type);
   });
   $("#addCommentButton").click(function() {
     var content = $('#contentComment').val();
@@ -28,21 +25,62 @@ $(document).ready(function(){
       objectID : id,
       objectType : type,
       content : content
-    }, function() {
-      $("#commentForJS").load("HTML/commentView.php" , {
-        objectID : id,
-        objectType : type
-      });
-    });
+    }, function() {load_comment(commentCount, id, type);}); 
     
   });
-  
+
   // Get the <span> element that closes the modal
   var span = document.getElementsByClassName("close")[0];
   
   // When the user clicks on <span> (x), close the modal
   span.onclick = function() {
     modal.style.display = "none";
+    commentCount = 5;
   }
-});
+
   
+});
+function load_more_comment() {
+  commentCount = commentCount + 4;
+  load_comment(commentCount,id, type);
+};
+function load_comment(commentCount, id, type) {
+  $("#commentForJS").load("HTML/commentView.php" , {
+    objectID : id,
+    objectType : type,
+    commentNewCount : commentCount
+  });  
+}
+function edit(myId, objectType) {
+  $("#commentForJS").load("DOMAINLOGIC/edit.dom.php" , {
+    objectID : myId,
+    objectType : objectType,
+    content : $('#newContent').val()
+  }, function() {
+    if (objectType == 'comment') {
+      load_comment(commentCount, id, type);
+    } else if (objectType == 'image') {
+      location.reload();
+    }
+      
+  });   
+}
+function deleteFunc(myId, objectType) {
+  $("#commentForJS").load("DOMAINLOGIC/delete.dom.php" , {
+    objectID : myId,
+    objectType : objectType
+  }, function() {
+    if (objectType == 'comment') {
+      load_comment(commentCount, id, type);
+    } else if (objectType == 'image') {
+      location.reload();
+    }
+      
+  });   
+}
+function edit_button_click(element, type) {
+  if (!$('#newContent').length){
+    $(element).after(`<textarea id="newContent"></textarea>`);
+    $('#newContent').after(`<button onclick="edit('${element.value}', '${type}');">submit</button>`);
+  } 
+}
--- JS/like.js
+++ JS/like.js
@@ -1,21 +1,20 @@
-$(document).ready(function(){
-    let likes = $("button[id^='IMG']");
-    for(let i = 0; i < likes.length; ++i){
-        likes[i].onclick = function(){
-            let action;
-            if(likes[i].classList.contains('btn-primary')){
-                action = 'remove';
-            }
-            else{
-                action = 'add';
-            }
-            likes[i].classList.toggle("btn-primary");
-            likes[i].classList.toggle("btn-outline-secondary");   
-            let imgID = likes[i].id.substring(3);
-            $(`#${imgID}`).load('DOMAINLOGIC/imageLike.dom.php' ,{
-                imgID: imgID,
-                action: action
-            });
-        }
+
+
+function checkLike(id, type) {
+    let element = $('button[id='+ id +']');
+    let action;
+    if(element.hasClass('btn-primary')){
+        action = 'remove';
+    }else{
+        action = 'add';
     }
-});
+    element.toggleClass("btn-primary");
+    element.toggleClass("btn-outline-secondary");   
+
+    let temporaire = id.substring(3);
+    $(`#${temporaire}`).load('DOMAINLOGIC/like.dom.php' ,{
+        objectID : temporaire,
+        objectType : type,
+        action: action
+    });
+}
