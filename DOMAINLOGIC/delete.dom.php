09c6756a5205381f7b2dcd71db577715a76004e9
MarcAndreHebert <58182354+MarcAndreHebert@users.noreply.github.com>
2019-12-13 14:26:31 -0500

    Add files via upload

--- DOMAINLOGIC/delete.dom.php
+++ DOMAINLOGIC/delete.dom.php
@@ -1,6 +1,7 @@
 <?php
     include_once __DIR__ . "/../CLASSES/COMMENT/comment.php";
     include_once __DIR__ . "/../CLASSES/IMAGE/image.php";
+    include_once __DIR__ . "/../CLASSES/ALBUM/album.php";
 
     session_start();
 
@@ -13,6 +14,9 @@
     } else if ($objectType == 'image') {
         $image = new Image();
         $image->delete_image($objectID);
+    } else if ($objectType == 'album') {
+        $album = new Album();
+        $album->delete_album($objectID);
     }
 
 ?>
