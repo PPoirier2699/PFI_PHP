8f6c30ed1b4c257a2809300ba114191ec1bc35cb
charleslefebvre <58051166+charleslefebvre@users.noreply.github.com>
2019-12-16 13:38:00 -0500

    Add files via upload

--- CLASSES/ALBUM/album.php
+++ CLASSES/ALBUM/album.php
@@ -1,6 +1,7 @@
 <?php
 
 include_once __DIR__ . "/albumTDG.php";
+include_once __DIR__ . "/../../UTILS/sessionHandler.php";
 
 class Album{
 
@@ -100,11 +101,41 @@ class Album{
         $TDG = AlbumTDG::getInstance();
         return $TDG->get_top_album($newAlbumCount);
     }
+    public function display_albums($res){
+        if(!empty($res)){      
+            foreach($res as $info){ 
+                echo "<div id='commentForJS' class='container border w-75 p-3 mt-5' style='float: left'>";
+                echo "<p><a style='text-decoration: none; color: black; font-size: 20px;' href='myProfile.php?userID=" . $info['userID'] . " '> " . $info['username'] ." </a></p>";
+                echo "<p><a style='text-decoration: none; color: black; font-size: 20px;' href='imageList.php?albumID=" . $info['id'] . "'>" .$info['title'] . "</a></p>";
+                echo "<img src=' " . $info['url'] . "'  alt='img' height='100'>";
+                echo "<p class='lead'>Description: " . $info['description'] . "</p>";
+                echo "<p class='lead'> " . $info['creationTime'] . "</p>";
+                if(validate_session() && $info["username"] == $_SESSION["userName"]) {			      
+                    echo "<button onClick='deleteFunc(" . $info['id'] . ", `album`);' type='button' class='btn'>Delete</button>";
+                } 
+                 echo "</div>";
+            }                                 
+        }   
+    }
     public function no_more_albums_to_display($albumNewCount,$res){
         if($albumNewCount > count($res)){
-            echo "<h6 style='position: absolute; bottom: 5%;'>No more albums</h6>";
-            echo"<script>$('#moreAlbums').remove();</script>";
+            echo"<script>$('#moreAlbums').remove();
+            $('#albums').append(`<h6 style=position: absolute; bottom: 0; float: left;'>No more albums</h6>`);</script>";
         }
+    }
+    public function display_album_search($res){
+        if(empty($res)){ 
+            echo "<h4>No albums corresponding to the research!</h4>";
+        }
+        else{            
+            foreach($res as $results){
+                echo "<h5 class='d-inline'><a style='text-decoration: none; color: black' href='imageList.php?albumID=" . $results['id'] ."'> " . $results['title'] . "</a></h5>";
+                echo "<a class='btn btn-primary' style='float: right' href='imageList.php?albumID=" . $results['id'] . " '>View album</a>";       
+                echo "<p class='lead'>" . $results['description'] . "</p>";
+                echo "<p class='lead'>" . $results['creationTime'] . "</p><br>";
+                echo "<br><button class='btn btn-light' style='position: absolute; left: 2%; bottom: 5%;' id='moreAlbums'>More albums</button>";
+            }
+        } 
     }
     public function search_album($like,$newAlbumCount){
         $TDG = AlbumTDG::getInstance();
