de6e451497da12865b2217dd2122990e0f356eb5
PPoirier2699 <57813534+PPoirier2699@users.noreply.github.com>
2019-12-16 16:32:32 -0500

    Add files via upload

---
+++ userAlbums.php
@@ -0,0 +1,14 @@
+<?php 
+    include_once __DIR__ . "./UTILS/sessionHandler.php";
+    session_start();
+
+    $title = "user Album";
+    $module = "userAlbumView.php";
+   
+    
+    $content = array();
+    array_push($content, $module);
+
+    require_once __DIR__ . "/HTML/masterpage.php";
+
+?>
