e8c137745e884b1b1dd03c9e82997791e61954dd
MarcAndreHebert <58182354+MarcAndreHebert@users.noreply.github.com>
2019-12-17 18:05:44 -0500

    Add files via upload

--- HTML/mainPageview.php
+++ HTML/mainPageview.php
@@ -1,20 +1,20 @@
 <script src="JS\loadFeedAlbums.js"></script>
 <script src="JS\imageView.js"></script>
-<script src='JS/delete.js'></script>
+<script src="JS\add.js"></script>
+<script src="JS\delete.js"></script>
+<script src="JS\edit.js"></script>
+<script src="JS\comment.js"></script>
 <h2>Main page content</h2>
-<div id="feed">
+<div id="albums">
 <?php
     include_once "./CLASSES/ALBUM/album.php";
-    include_once "./UTILS/sessionHandler.php";
-
+    include_once __DIR__ . "/../CLASSES/COMMENT/comment.php";
+    include_once __DIR__ . "/../CLASSES/LIKE/like.php";
+    include_once __DIR__ . "/../CLASSES/USER/user.php";
+    include_once __DIR__ . "/../UTILS/sessionHandler.php";
+    $comment = new Comment();
     $album = new Album;
     
     $res = $album->get_top_album(4);
-    $album->display_albums($res);
-
-?>
-</div>
-<div class="container w-75 mt-5" style='float: left; position: relative;' id="albums">
-    <button class="btn btn-light" style="position: absolute; left: 0;" id="moreAlbums">More albums</button>
-</div>
-
+    $album->display_album($res);
+ ?>
