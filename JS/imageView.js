23952e2c9fbaa889531823ac6d84613b96a6e262
MarcAndreHebert <58182354+MarcAndreHebert@users.noreply.github.com>
2019-12-17 21:05:38 -0500

    Update imageView.js

--- JS/imageView.js
+++ JS/imageView.js
@@ -9,29 +9,19 @@ $(document).ready(function(){
   $('.imageModal').click(function(){
       modal.style.display = "block";
       var newSrc = this.src;
+      type = $('#addComment').val();
       id = this.title;
-      type = $('#objectType').val();
+      $('#addComment').attr('value',this.title);
       modalImg.attr('title', id);
       $('#imageIDPostComment').attr('value', id);
       modalImg.attr('src', newSrc);      
-
-      load_comment(commentCount, id, type);
+      
+      load_comment('commentForJS', commentCount, id, 'image');
 
       span.onclick = function() {
         modal.style.display = "none";
         commentCount = 5;
       }
-  });
-  $("#addCommentButton").click(function() {
-    var content = $('#contentComment').val();
-    $('#contentComment').val("");
-
-    $("#commentForJS").load("DOMAINLOGIC/addComment.dom.php" , {
-      objectID : id,
-      objectType : type,
-      content : content
-    }, function() {load_comment(commentCount, id, type);}); 
-    
   });
 
   // Get the <span> element that closes the modal
@@ -42,35 +32,5 @@ $(document).ready(function(){
 
   
 });
-function load_more_comment() {
-  commentCount = commentCount + 4;
-  load_comment(commentCount,id, type);
-};
-function load_comment(commentCount, id, type) {
-  $("#commentForJS").load("HTML/commentView.php" , {
-    objectID : id,
-    objectType : type,
-    commentNewCount : commentCount
-  });  
-}
-function edit(myId, objectType) {
-  $("#commentForJS").load("DOMAINLOGIC/edit.dom.php" , {
-    objectID : myId,
-    objectType : objectType,
-    content : $('#newContent').val()
-  }, function() {
-    if (objectType == 'comment') {
-      load_comment(commentCount, id, type);
-    } else if (objectType == 'image') {
-      location.reload();
-    }
-      
-  });   
-}
 
-function edit_button_click(element, type) {
-  if (!$('#newContent').length){
-    $(element).after(`<textarea id="newContent"></textarea>`);
-    $('#newContent').after(`<button onclick="edit('${element.value}', '${type}');">submit</button>`);
-  } 
-}
+
